493f073581c2358c365189276521dae31a20a183
Paul "LeoNerd" Evans <leonerd@leonerd.org.uk>
2007-12-01 02:19:16 +0000

    Parse ESC 0x3d and ESC 0x3e as keypad mode settings; apply to sent keyboard strings

--- ecma48_internal.h
+++ ecma48_internal.h
@@ -19,6 +19,10 @@ struct ecma48_s
   GString *inbuffer;
   GString *outbuffer;
   ecma48_state_t *state;
+
+  struct {
+    int keypad:1;
+  } mode;
 };
 
 size_t ecma48_parser_interpret_bytes(ecma48_t *e48, char *bytes, size_t len);
@@ -29,6 +33,7 @@ void ecma48_push_output_sprintf(ecma48_t *e48, char *format, ...);
 
 int ecma48_state_on_text(ecma48_t *e48, int codepoints[], int npoints);
 int ecma48_state_on_control(ecma48_t *e48, char control);
+int ecma48_state_on_escape(ecma48_t *e48, char escape);
 int ecma48_state_on_csi(ecma48_t *e48, int *args, int argcount, char command);
 
 #endif
