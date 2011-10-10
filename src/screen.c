958ba74492459d08b662ca67f7f36702c1f1aa7a
Paul LeoNerd Evans <leonerd@leonerd.org.uk>
2011-10-10 19:29:43 +0100

    Abstract away malloc/free behaviours behind a pair of VTerm-overridable functions

--- src/screen.c
+++ src/screen.c
@@ -56,7 +56,7 @@ static inline ScreenCell *getcell(VTermScreen *screen, int row, int col)
 
 static ScreenCell *realloc_buffer(VTermScreen *screen, ScreenCell *buffer, int new_rows, int new_cols)
 {
-  ScreenCell *new_buffer = g_new0(ScreenCell, new_rows * new_cols);
+  ScreenCell *new_buffer = vterm_allocator_new(screen->vt, sizeof(ScreenCell) * new_rows * new_cols);
 
   for(int row = 0; row < new_rows; row++) {
     for(int col = 0; col < new_cols; col++) {
@@ -72,7 +72,7 @@ static ScreenCell *realloc_buffer(VTermScreen *screen, ScreenCell *buffer, int n
   }
 
   if(buffer)
-    g_free(buffer);
+    vterm_allocator_free(screen->vt, buffer);
 
   return new_buffer;
 }
@@ -304,7 +304,7 @@ static VTermStateCallbacks state_cbs = {
 
 static VTermScreen *screen_new(VTerm *vt)
 {
-  VTermScreen *screen = g_new0(VTermScreen, 1);
+  VTermScreen *screen = vterm_allocator_new(vt, sizeof(VTermScreen));
   int rows, cols;
 
   vterm_get_size(vt, &rows, &cols);
