82487f39f955e1afc3b178bf5075fb19711579e6
Paul LeoNerd Evans <leonerd@leonerd.org.uk>
2012-12-06 01:41:40 +0000

    Recognise keypad input keys, implement DECKPAM and DECKPNM

--- src/input.c
+++ src/input.c
@@ -58,9 +58,11 @@ typedef struct {
     KEYCODE_LITERAL,
     KEYCODE_TAB,
     KEYCODE_ENTER,
+    KEYCODE_SS3,
     KEYCODE_CSI,
     KEYCODE_CSI_CURSOR,
     KEYCODE_CSINUM,
+    KEYCODE_KEYPAD,
   } type;
   char literal;
   int csinum;
@@ -85,7 +87,9 @@ static keycodes_s keycodes[] = {
   { KEYCODE_CSI_CURSOR, 'F' }, // END
   { KEYCODE_CSINUM, '~', 5 },  // PAGEUP
   { KEYCODE_CSINUM, '~', 6 },  // PAGEDOWN
+};
 
+static keycodes_s keycodes_fn[] = {
   { KEYCODE_NONE },            // F0 - shouldn't happen
   { KEYCODE_CSI_CURSOR, 'P' }, // F1
   { KEYCODE_CSI_CURSOR, 'Q' }, // F2
@@ -101,6 +105,27 @@ static keycodes_s keycodes[] = {
   { KEYCODE_CSINUM, '~', 24 }, // F12
 };
 
+static keycodes_s keycodes_kp[] = {
+  { KEYCODE_KEYPAD, '0', 'p' }, // KP_0
+  { KEYCODE_KEYPAD, '1', 'q' }, // KP_1
+  { KEYCODE_KEYPAD, '2', 'r' }, // KP_2
+  { KEYCODE_KEYPAD, '3', 's' }, // KP_3
+  { KEYCODE_KEYPAD, '4', 't' }, // KP_4
+  { KEYCODE_KEYPAD, '5', 'u' }, // KP_5
+  { KEYCODE_KEYPAD, '6', 'v' }, // KP_6
+  { KEYCODE_KEYPAD, '7', 'w' }, // KP_7
+  { KEYCODE_KEYPAD, '8', 'x' }, // KP_8
+  { KEYCODE_KEYPAD, '9', 'y' }, // KP_9
+  { KEYCODE_KEYPAD, '*', 'j' }, // KP_MULT
+  { KEYCODE_KEYPAD, '+', 'k' }, // KP_PLUS
+  { KEYCODE_KEYPAD, ',', 'l' }, // KP_COMMA
+  { KEYCODE_KEYPAD, '-', 'm' }, // KP_MINUS
+  { KEYCODE_KEYPAD, '.', 'n' }, // KP_PERIOD
+  { KEYCODE_KEYPAD, '/', 'o' }, // KP_DIVIDE
+  { KEYCODE_KEYPAD, '\n', 'M' }, // KP_ENTER
+  { KEYCODE_KEYPAD, '=', 'X' }, // KP_EQUAL
+};
+
 void vterm_input_push_key(VTerm *vt, VTermModifier mod, VTermKey key)
 {
   /* Since Shift-Enter and Shift-Backspace are too easy to mistype
@@ -109,13 +134,25 @@ void vterm_input_push_key(VTerm *vt, VTermModifier mod, VTermKey key)
   if((key == VTERM_KEY_ENTER || key == VTERM_KEY_BACKSPACE) && mod == VTERM_MOD_SHIFT)
     mod = 0;
 
-  if(key == VTERM_KEY_NONE || key >= VTERM_KEY_MAX)
+  if(key == VTERM_KEY_NONE)
     return;
 
-  if(key >= sizeof(keycodes)/sizeof(keycodes[0]))
-    return;
-
-  keycodes_s k = keycodes[key];
+  keycodes_s k;
+  if(key < VTERM_KEY_FUNCTION_0) {
+    if(key >= sizeof(keycodes)/sizeof(keycodes[0]))
+      return;
+    k = keycodes[key];
+  }
+  else if(key >= VTERM_KEY_FUNCTION_0 && key <= VTERM_KEY_FUNCTION_MAX) {
+    if((key - VTERM_KEY_FUNCTION_0) >= sizeof(keycodes_fn)/sizeof(keycodes_fn[0]))
+      return;
+    k = keycodes_fn[key - VTERM_KEY_FUNCTION_0];
+  }
+  else if(key >= VTERM_KEY_KP_0) {
+    if((key - VTERM_KEY_KP_0) >= sizeof(keycodes_kp)/sizeof(keycodes_kp[0]))
+      return;
+    k = keycodes_kp[key - VTERM_KEY_KP_0];
+  }
 
   switch(k.type) {
   case KEYCODE_NONE:
@@ -128,7 +165,7 @@ void vterm_input_push_key(VTerm *vt, VTermModifier mod, VTermKey key)
     else if(mod & VTERM_MOD_SHIFT)
       vterm_push_output_sprintf_ctrl(vt, C1_CSI, "1;%dZ", mod+1);
     else
-      goto literal;
+      goto case_LITERAL;
     break;
 
   case KEYCODE_ENTER:
@@ -136,24 +173,24 @@ void vterm_input_push_key(VTerm *vt, VTermModifier mod, VTermKey key)
     if(vt->state->mode.newline)
       vterm_push_output_sprintf(vt, "\r\n");
     else
-      goto literal;
+      goto case_LITERAL;
     break;
 
-literal:
-  case KEYCODE_LITERAL:
+  case KEYCODE_LITERAL: case_LITERAL:
     if(mod & (VTERM_MOD_SHIFT|VTERM_MOD_CTRL))
       vterm_push_output_sprintf_ctrl(vt, C1_CSI, "%d;%du", k.literal, mod+1);
     else
       vterm_push_output_sprintf(vt, mod & VTERM_MOD_ALT ? "\e%c" : "%c", k.literal);
     break;
 
-  case KEYCODE_CSI_CURSOR:
-    if(vt->state->mode.cursor && mod == 0) {
+  case KEYCODE_SS3: case_SS3:
+    if(mod == 0)
       vterm_push_output_sprintf_ctrl(vt, C1_SS3, "%c", k.literal);
-      break;
-    }
-    /* else FALLTHROUGH */
-  case KEYCODE_CSI:
+    else
+      goto case_CSI;
+    break;
+
+  case KEYCODE_CSI: case_CSI:
     if(mod == 0)
       vterm_push_output_sprintf_ctrl(vt, C1_CSI, "%c", k.literal);
     else
@@ -166,5 +203,19 @@ literal:
     else
       vterm_push_output_sprintf_ctrl(vt, C1_CSI, "%d;%d%c", k.csinum, mod + 1, k.literal);
     break;
+
+  case KEYCODE_CSI_CURSOR:
+    if(vt->state->mode.cursor)
+      goto case_SS3;
+    else
+      goto case_CSI;
+
+  case KEYCODE_KEYPAD:
+    if(vt->state->mode.keypad) {
+      k.literal = k.csinum;
+      goto case_SS3;
+    }
+    else
+      goto case_LITERAL;
   }
 }
