b71bbeb072a54f867db6fa67d4536f6161e2b07a
Paul LeoNerd Evans <leonerd@leonerd.org.uk>
2015-10-16 15:17:35 +0100

    Add 'query' option to all the DEC modes in vterm-ctrl

--- bin/vterm-ctrl.c
+++ bin/vterm-ctrl.c
@@ -1,8 +1,12 @@
+#define _XOPEN_SOURCE 500  /* strdup */
+
 #include <stdbool.h>
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
 #define streq(a,b) (strcmp(a,b)==0)
+
+#include <termios.h>
 
 static char *getvalue(int *argip, int argc, char *argv[])
 {
@@ -27,26 +31,84 @@ static int getchoice(int *argip, int argc, char *argv[], const char *options[])
   exit(1);
 }
 
-static bool getbool(int *argip, int argc, char *argv[])
+typedef enum {
+  OFF,
+  ON,
+  QUERY,
+} BoolQuery;
+
+static BoolQuery getboolq(int *argip, int argc, char *argv[])
 {
-  return getchoice(argip, argc, argv, (const char *[]){"off", "on", NULL});
+  return getchoice(argip, argc, argv, (const char *[]){"off", "on", "query", NULL});
 }
 
 static char *helptext[] = {
   "reset",
   "keypad [app|num]",
-  "screen [off|on]",
-  "cursor [off|on]",
-  "curblink [off|on]",
+  "screen [off|on|query]",
+  "cursor [off|on|query]",
+  "curblink [off|on|query]",
   "curshape [block|under|bar]",
   "mouse [off|click|clickdrag|motion]",
-  "altscreen [off|on]",
-  "bracketpaste [off|on]",
+  "altscreen [off|on|query]",
+  "bracketpaste [off|on|query]",
   "icontitle [STR]",
   "icon [STR]",
   "title [STR]",
   NULL
 };
+
+static bool seticanon(bool icanon, bool echo)
+{
+  struct termios termios;
+
+  tcgetattr(0, &termios);
+
+  bool ret = (termios.c_lflag & ICANON);
+
+  if(icanon) termios.c_lflag |=  ICANON;
+  else       termios.c_lflag &= ~ICANON;
+
+  if(echo) termios.c_lflag |=  ECHO;
+  else     termios.c_lflag &= ~ECHO;
+
+  tcsetattr(0, TCSANOW, &termios);
+
+  return ret;
+}
+
+static char *read_csi()
+{
+  /* TODO: This really should be a more robust CSI parser
+   */
+  char c;
+
+  /* await CSI - 8bit or 2byte 7bit form */
+  bool in_esc = false;
+  while((c = getchar())) {
+    if(c == 0x9b)
+      break;
+    if(in_esc && c == 0x5b)
+      break;
+    if(!in_esc && c == 0x1b)
+      in_esc = true;
+    else
+      in_esc = false;
+  }
+
+  char csi[32];
+  int i = 0;
+  for(; i < sizeof(csi)-1; i++) {
+    c = csi[i] = getchar();
+    if(c >= 0x40 && c <= 0x7e)
+      break;
+  }
+  csi[++i] = 0;
+
+  // TODO: returns longer than 32?
+
+  return strdup(csi);
+}
 
 static void usage(int exitcode)
 {
@@ -60,9 +122,51 @@ static void usage(int exitcode)
   exit(exitcode);
 }
 
-static void do_dec_mode(int mode, bool on)
+static void do_dec_mode(int mode, BoolQuery val, const char *name)
 {
-  printf("\e[?%d%c", mode, on ? 'h' : 'l');
+  switch(val) {
+    case OFF:
+    case ON:
+      printf("\e[?%d%c", mode, val == ON ? 'h' : 'l');
+      break;
+
+    case QUERY: {
+      printf("\e[?%d$p", mode);
+
+      char *s = NULL;
+      do {
+        if(s)
+          free(s);
+        s = read_csi();
+
+        /* expect "?" mode ";" value "$y" */
+
+        int reply_mode, reply_value;
+        char reply_cmd;
+        /* If the sscanf format string ends in a literal, we can't tell from
+         * its return value if it matches. Hence we'll %c the cmd and check it
+         * explicitly
+         */
+        if(sscanf(s, "?%d;%d$%c", &reply_mode, &reply_value, &reply_cmd) < 3)
+          continue;
+        if(reply_cmd != 'y')
+          continue;
+
+        if(reply_mode != mode)
+          continue;
+
+        if(reply_value == 1 || reply_value == 3)
+          printf("%s on\n", name);
+        else if(reply_value == 2 || reply_value == 4)
+          printf("%s off\n", name);
+
+        break;
+      } while(1);
+
+      free(s);
+      break;
+    }
+  }
 }
 
 int main(int argc, char *argv[])
@@ -71,6 +175,8 @@ int main(int argc, char *argv[])
 
   if(argc == 1)
     usage(0);
+
+  bool wasicanon = seticanon(false, false);
 
   while(argi < argc) {
     const char *arg = argv[argi++];
@@ -87,13 +193,13 @@ int main(int argc, char *argv[])
       }
     }
     else if(streq(arg, "screen")) {
-      do_dec_mode(5, getbool(&argi, argc, argv));
+      do_dec_mode(5, getboolq(&argi, argc, argv), "screen");
     }
     else if(streq(arg, "cursor")) {
-      do_dec_mode(25, getbool(&argi, argc, argv));
+      do_dec_mode(25, getboolq(&argi, argc, argv), "cursor");
     }
     else if(streq(arg, "curblink")) {
-      do_dec_mode(12, getbool(&argi, argc, argv));
+      do_dec_mode(12, getboolq(&argi, argc, argv), "curblink");
     }
     else if(streq(arg, "curshape")) {
       // TODO: This ought to query the current value of DECSCUSR because it
@@ -114,10 +220,10 @@ int main(int argc, char *argv[])
       }
     }
     else if(streq(arg, "altscreen")) {
-      do_dec_mode(1049, getbool(&argi, argc, argv));
+      do_dec_mode(1049, getboolq(&argi, argc, argv), "altscreen");
     }
     else if(streq(arg, "bracketpaste")) {
-      do_dec_mode(2004, getbool(&argi, argc, argv));
+      do_dec_mode(2004, getboolq(&argi, argc, argv), "bracketpaste");
     }
     else if(streq(arg, "icontitle")) {
       printf("\e]0;%s\a", getvalue(&argi, argc, argv));
@@ -133,4 +239,6 @@ int main(int argc, char *argv[])
       exit(1);
     }
   }
+
+  seticanon(wasicanon, true);
 }
