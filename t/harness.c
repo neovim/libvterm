6c93aef9965d7fcd3e32e42670d6031e8c1f07a0
Paul "LeoNerd" Evans <leonerd@leonerd.org.uk>
2022-02-24 00:42:28 +0000

    WIP: reflow attempt

--- t/harness.c
+++ t/harness.c
@@ -717,6 +717,9 @@ int main(int argc, char **argv)
         case 'b':
           want_screen_scrollback = sense;
           break;
+        case 'r':
+          vterm_screen_set_reflow(screen, sense);
+          break;
         default:
           fprintf(stderr, "Unrecognised WANTSCREEN flag '%c'\n", line[i]);
         }
